d84e32f4f1dbf5a291888dc7735c727cebf2629d
$hripal <$hripal@192.168.0.9>
2018-12-25 22:48:34 -0500

    changed AbstractTestBase to include Firefox

--- selenium/src/main/java/com/framework/selenium/AbstractTestBase.java
+++ selenium/src/main/java/com/framework/selenium/AbstractTestBase.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
 
 import com.framework.selenium.common.ConfigProperties;
@@ -26,7 +27,7 @@ public class AbstractTestBase {
 
 		if(StringUtils.lowerCase(browserName).equalsIgnoreCase("firefox")) {
 			System.setProperty("webdriver.geco.driver", chromeDriverPath);
-			this.driver = new ChromeDriver();
+			this.driver = new FirefoxDriver();
 		}
 		else {
 			System.setProperty("webdriver.chrome.driver", chromeDriverPath);
